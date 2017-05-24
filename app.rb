7bd3fabc4466c4f2e25c2bdb31d41eaab43fcd8d
Rory Devane <rory.devane@gmail.com>
2017-05-24 10:46:41 +0100

    switched from slim to erb

--- app.rb
+++ app.rb
@@ -1,27 +1,5 @@
 require 'sinatra'
-# require 'sinatra/activerecord'
-# require './config/environments'
-# require 'slim'
-# Dir[File.join(File.dirname(__FILE__), './lib/clients', '*.rb')].each {|file| require file }
-# Dir[File.join(File.dirname(__FILE__), './lib/helpers', '*.rb')].each {|file| require file }
-# Dir[File.join(File.dirname(__FILE__), './lib/models', '*.rb')].each {|file| require file }
-
-set :haml, :format => :html5
-
-# helpers SampleHelper, DateHelper, EchoHelper
 
 get '/' do
-  slim :index
-end
-
-get '/second' do
-  slim :second
-end
-
-get '/sample' do
-  slim :sample
-end
-
-get '/form' do
-  slim :form
+  erb :index
 end
