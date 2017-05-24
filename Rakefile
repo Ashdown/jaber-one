b4db1d016323860e66d6d9f7b7e8c6c5143422ac
Rory Devane <rory.devane@gmail.com>
2017-05-24 10:28:41 +0100

    removing spec and features

--- Rakefile
+++ Rakefile
@@ -1,6 +1,4 @@
 require './app'
-require 'sinatra/activerecord/rake'
-require 'rspec/core/rake_task'
 require 'jasmine'
 require 'jshintrb/jshinttask'
 
@@ -12,11 +10,5 @@ Jshintrb::JshintTask.new :jshint do |task|
   task.options = Jshintrb::Lint::DEFAULTS.merge({:predef => ['jQuery']})
 end
 
-RSpec::Core::RakeTask.new(:spec)
-
-RSpec::Core::RakeTask.new(:feature) do |task|
-  task.pattern = 'features/*spec.rb'
-end
-
-task :default=>[:spec, :feature, :jshint, :'jasmine:ci']
+task :default=>[:jshint, :'jasmine:ci']
 
--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require 'sinatra'
-require 'sinatra/activerecord'
-require './config/environments'
-require 'slim'
-Dir[File.join(File.dirname(__FILE__), './lib/clients', '*.rb')].each {|file| require file }
-Dir[File.join(File.dirname(__FILE__), './lib/helpers', '*.rb')].each {|file| require file }
-Dir[File.join(File.dirname(__FILE__), './lib/models', '*.rb')].each {|file| require file }
+# require 'sinatra/activerecord'
+# require './config/environments'
+# require 'slim'
+# Dir[File.join(File.dirname(__FILE__), './lib/clients', '*.rb')].each {|file| require file }
+# Dir[File.join(File.dirname(__FILE__), './lib/helpers', '*.rb')].each {|file| require file }
+# Dir[File.join(File.dirname(__FILE__), './lib/models', '*.rb')].each {|file| require file }
 
 set :haml, :format => :html5
 
-helpers SampleHelper, DateHelper, EchoHelper
+# helpers SampleHelper, DateHelper, EchoHelper
 
 get '/' do
   slim :index
@@ -24,18 +24,4 @@ end
 
 get '/form' do
   slim :form
-end
-
-get '/list' do
-  @samples = Sample.all
-  slim :list
-end
-
-post '/submit' do
-  @sample = Sample.new(params[:sample])
-  if @sample.save
-    redirect '/list'
-  else
-    'An error has occured'
-  end
 end
