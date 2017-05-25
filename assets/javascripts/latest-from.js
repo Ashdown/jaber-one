5979d820c5af791877ba4dfa5ef368967d31cb5a
Rory Devane <rory.devane@gmail.com>
2017-05-25 14:40:01 +0100

    styled desktop version of the homepage

---
+++ assets/javascripts/latest-from.js
@@ -0,0 +1,12 @@
+(function($){
+    'use strict';
+    $('.latest-from .news-articles').slick({
+        infinite: true,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        centerMode: false,
+        variableWidth: true
+    });
+
+})(jQuery);
