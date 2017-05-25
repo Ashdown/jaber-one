41df34dba4bfedcd7809882322af75536532f529
Rory Devane <rory.devane@gmail.com>
2017-05-25 15:10:17 +0100

    styled desktop version of latest from component

--- assets/javascripts/latest-from.js
+++ assets/javascripts/latest-from.js
@@ -6,7 +6,14 @@
         arrows: false,
         dots: true,
         centerMode: false,
-        variableWidth: true
+        variableWidth: true,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 1023,
+                settings: "unslick"
+            }
+        ]
     });
 
 })(jQuery);
