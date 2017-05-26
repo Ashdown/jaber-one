42e39419fd4693f2d18959e5a7d3538ddf7cbfd7
Rory Devane <rory.devane@gmail.com>
2017-05-26 09:04:25 +0100

    styled the mobile version of the header search bar

--- assets/javascripts/header.js
+++ assets/javascripts/header.js
@@ -6,11 +6,18 @@
     $this.find('.menu-link').click(function(event){
         event.preventDefault();
         $this.toggleClass('open-menu');
+
     });
+
+    var searchBarActive = false;
 
     $this.find('.search-link').click(function(event){
         event.preventDefault();
         $this.find('.header-search').toggleClass('visible');
+        if(!searchBarActive) {
+            $this.find('.keyword-field').focus();
+        }
+        searchBarActive = !searchBarActive;
     });
 
 })(jQuery);
