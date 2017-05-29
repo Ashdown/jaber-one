46c234de57b41bbb0f3dc13504a395618fa7c9cf
Rory Devane <rory.devane@gmail.com>
2017-05-29 17:18:08 +0100

    set height of disc swithcer

--- assets/javascripts/disc-switcher.js
+++ assets/javascripts/disc-switcher.js
@@ -1,12 +1,20 @@
-(function($) {
+(function ($) {
     'use strict';
 
     var $this = $('.disc-switcher');
 
-    $this.find('.disc-link').on('click', function(event) {
+    function setContainerHeight($currentContainer) {
+        $this.css('padding-bottom', $currentContainer.height());
+    }
+
+    $this.find('.disc-link').on('click', function (event) {
         event.preventDefault();
-       $this.find('.disc-item').removeClass('selected');
-        $(event.currentTarget).closest('.disc-item').addClass('selected');
+        var $currentItem = $(event.currentTarget).closest('.disc-item');
+        $this.find('.disc-item').removeClass('selected');
+        $currentItem.addClass('selected');
+        setContainerHeight($currentItem.find('.container'));
     });
+
+    setContainerHeight($this.find('.disc-item.selected .container'));
 
 })(jQuery);
