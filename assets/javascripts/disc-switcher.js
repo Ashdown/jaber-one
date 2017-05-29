54e11a95cda6a9f44da9be3fbd8d60f7c38b619e
Rory Devane <rory.devane@gmail.com>
2017-05-29 19:44:04 +0100

    added read more link to bottom of disc content

--- assets/javascripts/disc-switcher.js
+++ assets/javascripts/disc-switcher.js
@@ -4,6 +4,7 @@
     var $this = $('.disc-switcher');
 
     function setContainerHeight($currentContainer) {
+        console.log('set height', $currentContainer.height());
         $this.css('padding-bottom', $currentContainer.height());
     }
 
@@ -13,6 +14,21 @@
         $this.find('.disc-item').removeClass('selected');
         $currentItem.addClass('selected');
         setContainerHeight($currentItem.find('.container'));
+    });
+
+    $this.find('.read-more-link').on('click', function(event) {
+        event.preventDefault();
+        var $container = $(event.currentTarget).closest('.container');
+        $container.addClass('open');
+        window.setTimeout(function() {
+            $container.find('.white-button-link').css('display', 'none');
+            setContainerHeight($container);
+        }, 200);
+
+    });
+
+    $(window).resize(function() {
+        setContainerHeight($this.find('.disc-item.selected .container'));
     });
 
     setContainerHeight($this.find('.disc-item.selected .container'));
