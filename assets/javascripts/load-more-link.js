e1148c4a77490485ea0718f187f24105cbf6faf7
Rory Devane <rory.devane@gmail.com>
2017-05-31 10:40:30 +0100

    load more link now works

---
+++ assets/javascripts/load-more-link.js
@@ -0,0 +1,32 @@
+(function($) {
+
+    'use strict';
+
+    var $this = $('.load-more-link');
+
+    $this.on('click', function(event) {
+        event.preventDefault();
+        var $currentTarget = $(event.currentTarget),
+            $list = $($this.attr('href')),
+            $invisibleImageLinks = $list.find('.article-image-link.hidden');
+        for(var i = 0; i < 3; i++) {
+            $invisibleImageLinks.eq(i).removeClass('hidden');
+        }
+        if($invisibleImageLinks.length <= 3) {
+            $currentTarget.addClass('hidden');
+        }
+    });
+
+    //turn on links if they are needed
+
+    $this.each(function() {
+        var $list = $($(this).attr('href')),
+            $invisibleImageLinks = $list.find('.article-image-link.hidden');
+        if($invisibleImageLinks.length > 0) {
+            $(this).removeClass('hidden');
+        }
+
+    })
+
+
+})(jQuery);
