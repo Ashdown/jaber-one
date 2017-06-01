b1ace129443d7c5c9693418d69e5c840b7c69927
Rory Devane <rory.devane@gmail.com>
2017-06-01 11:01:01 +0100

    can now deep link to news menu contents

--- assets/javascripts/news-menu.js
+++ assets/javascripts/news-menu.js
@@ -2,20 +2,42 @@
 
     'use strict';
 
-    var $this = $('.news-menu');
+    var $this = $('.news-menu'),
+        $page  = $('.page');
 
-    $this.find('.link').on('click', function(event) {
-        event.preventDefault();
-        var $newContent = $($(event.currentTarget).attr('href'));
+    function openContent(href) {
+        var $newContent = $(href.slice(href.indexOf('#')));
         $('.news-menu-content').removeClass('visible');
         $newContent.addClass('visible');
         $this.removeClass('open');
-    });
-
+    }
 
     $this.find('.menu-link').on('click', function(event) {
         event.preventDefault();
         $this.toggleClass('open');
     });
+
+    $this.find('.main-menu .link').on('click', function(event) {
+
+        var href = $(event.currentTarget).attr('href');
+
+        if($page.hasClass('news-page')) {
+            event.preventDefault();
+            openContent(href);
+        }
+    });
+
+    //open content if page is deeplinked
+
+    if($page.hasClass('news-page')) {
+        var href = window.location.href;
+        if(href.indexOf('#') > 0) {
+            openContent(href);
+            setTimeout(function() {
+                window.scrollTo(0, 0);
+            }, 1);
+        }
+    }
+
 
 })(jQuery);
