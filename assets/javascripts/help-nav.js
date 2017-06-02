59f06b233d1e441641cb9c9f5f30ed5831f6e401
Rory Devane <rory.devane@gmail.com>
2017-06-02 14:40:46 +0100

    styled mobile version of the help page links list

---
+++ assets/javascripts/help-nav.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+    'use strict';
+
+    var $this = $('.help-nav');
+
+    $this.slick({
+        infinite: true,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        variableWidth: true
+    });
+
+
+})(jQuery);
