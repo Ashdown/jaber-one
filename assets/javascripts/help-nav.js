d9f11ed9e274f1c9820492e3f692c902f6ebc46f
Rory Devane <rory.devane@gmail.com>
2017-06-02 15:06:45 +0100

    styled desktop version of help page

--- assets/javascripts/help-nav.js
+++ assets/javascripts/help-nav.js
@@ -2,16 +2,35 @@
 
     'use strict';
 
-    var $this = $('.help-nav');
+    var $this = $('.help-nav'),
+        slickCreated = false,
+        slickSettings = {
+            infinite: true,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            centerMode: true,
+            variableWidth: true
+        };
 
-    $this.slick({
-        infinite: true,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        centerMode: true,
-        variableWidth: true
-    });
+    var checkForSlick = function() {
+        if (window.innerWidth < 1024) {
+
+            if(!slickCreated) {
+                slickCreated = true;
+                $this.slick(slickSettings);
+            }
+            return;
+        }
+        if(slickCreated) {
+            slickCreated = false;
+            return $this.slick('unslick');
+        }
+    };
+
+    $(window).on('resize', checkForSlick);
+
+    checkForSlick();
 
 
 })(jQuery);
