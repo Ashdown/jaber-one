f250711f684515347b32d015a74344c76b131584
Rory Devane <rory.devane@gmail.com>
2017-06-05 15:35:07 +0100

    styled mobile version of video results component

---
+++ assets/javascripts/video-results.js
@@ -0,0 +1,33 @@
+(function($){
+    var $this = $('.video-results'),
+        $list = $this.find('.video-results-list'),
+        slickCreated = false,
+        slickSettings = {
+            infinite: true,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            centerMode: true,
+            variableWidth: true
+        };
+
+    var checkForSlick = function() {
+        if (window.innerWidth < 1024) {
+
+            if(!slickCreated) {
+                slickCreated = true;
+                $list.slick(slickSettings);
+            }
+            return;
+        }
+        if(slickCreated) {
+            slickCreated = false;
+            return $list.slick('unslick');
+        }
+    };
+
+    $(window).on('resize', checkForSlick);
+
+    checkForSlick();
+
+})(jQuery);
