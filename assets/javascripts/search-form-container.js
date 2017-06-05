4dde3db1567cb9661b8900fce3a279ea149d9a81
Rory Devane <rory.devane@gmail.com>
2017-06-05 14:21:34 +0100

    styled desktop version of search form

---
+++ assets/javascripts/search-form-container.js
@@ -0,0 +1,8 @@
+(function($){
+    var $this = $('.search-form-container');
+    $this.find('.open-filters-link').on('click', function(event) {
+        event.preventDefault()
+        $this.toggleClass('filters-open');
+    });
+
+})(jQuery);
