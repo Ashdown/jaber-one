29d5170fc366e448b77e13176e71c13e09c76f5a
Rory Devane <rory.devane@gmail.com>
2017-06-06 14:26:20 +0100

    show hide contact information panel

---
+++ assets/javascripts/contact-details.js
@@ -0,0 +1,24 @@
+(function($){
+    'use strict';
+
+    var $this = $('.contact-details');
+
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    //get query param
+
+    if(getParameterByName('show-contact-information') === 'true') {
+        $this.find('#contact-list-container').addClass('hidden');
+        $this.find('#contact-information-container').removeClass('hidden');
+    }
+
+
+})(jQuery);
