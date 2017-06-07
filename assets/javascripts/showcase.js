1be404405f1361885295f8eca0d97d7b63538499
Rory Devane <rory.devane@gmail.com>
2017-06-07 09:36:17 +0100

    added hover and animate controlled by js

--- assets/javascripts/showcase.js
+++ assets/javascripts/showcase.js
@@ -1,12 +1,36 @@
 (function($){
     'use strict';
 
-    var $this = $('.showcase');
+    var $this = $('.showcase'),
+        $links = $this.find('.link'),
+        scrollOffset = 150;
+
+    //function checks if an element has scrolled into view
+    function elementScrolled(elem) {
+        var docViewTop = $(window).scrollTop(),
+            docViewBottom = docViewTop + $(window).height(),
+            elemTop = $(elem).offset().top + scrollOffset,
+            elemBottom = elemTop + $(elem).height() - scrollOffset;
+
+        return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+    }
+
+    $(window).scroll(function(){
+        $links.each(function(){
+            var $link = $(this);
+            if(elementScrolled($link)) {
+                $link.addClass('animate');
+            } else {
+                $link.removeClass('animate');
+            }
+        });
+
+    });
 
     $this.find('.link').mouseenter(function(event) {
-        $(event.currentTarget).addClass('animate');
+        $(event.currentTarget).addClass('hover');
     }).mouseleave(function(event){
-        $(event.currentTarget).removeClass('animate');
+        $(event.currentTarget).removeClass('hover');
     });
 
 })(jQuery);
